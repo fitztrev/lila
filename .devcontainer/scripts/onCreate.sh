b57f66b5dbe77d9c7f5527c5b50c7bd71d59f7e3
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2022-11-21 19:57:23 +0000

    add other commands

--- .devcontainer/scripts/onCreate.sh
+++ .devcontainer/scripts/onCreate.sh
@@ -2,31 +2,27 @@
 
 echo '####### starting onCreate.sh ###################'
 
-whoami
+cd /workspaces
+git clone https://github.com/lichess-org/lila-ws.git
+git clone https://github.com/lichess-org/lila-db-seed.git
+git clone https://github.com/lichess-org/fishnet.git --recursive
+git clone https://github.com/lichess-org/lila-fishnet.git
+git clone https://github.com/lichess-org/pgn-viewer.git
 
-node --version
+## Setup initial database and seed test data (users, games, puzzles, etc)
+mkdir -p /workspaces/mongodb-data
+sudo mongod --fork --dbpath /workspaces/mongodb-data --logpath /var/log/mongod.log
+mongo lichess /workspaces/lila/bin/mongodb/indexes.js
+python3.9 /workspaces/lila-db-seed/spamdb/spamdb.py --drop all
+sudo killall mongod
 
-# cd /workspaces
-# git clone https://github.com/lichess-org/lila-ws.git
-# git clone https://github.com/lichess-org/lila-db-seed.git
-# git clone https://github.com/lichess-org/fishnet.git --recursive
-# git clone https://github.com/lichess-org/lila-fishnet.git
-# git clone https://github.com/lichess-org/pgn-viewer.git
+cd /workspaces/lila-ws
+sbt compile
 
-# ## Setup initial database and seed test data (users, games, puzzles, etc)
-# mkdir -p /workspaces/mongodb-data
-# sudo mongod --fork --dbpath /workspaces/mongodb-data --logpath /var/log/mongod.log
-# mongo lichess /workspaces/lila/bin/mongodb/indexes.js
-# python3.9 /workspaces/lila-db-seed/spamdb/spamdb.py --drop all
-# sudo killall mongod
+cd /workspaces/lila
+sbt bloopInstall
+bloop compile lila
 
-# cd /workspaces/lila-ws
-# sbt compile
-
-# cd /workspaces/lila
-# sbt bloopInstall
-# bloop compile lila
-
-# ./ui/build
+./ui/build
 
 echo '####### ending onCreate.sh ###################'
