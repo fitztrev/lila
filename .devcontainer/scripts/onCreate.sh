3e52417b2759d57a26ed26ee5025c3fe9f0ed2c3
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2022-11-21 18:30:29 +0000

    debug

--- .devcontainer/scripts/onCreate.sh
+++ .devcontainer/scripts/onCreate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-su - lichess
+whoami
 
 cd /workspaces
 git clone https://github.com/lichess-org/lila-ws.git
@@ -8,6 +8,8 @@ git clone https://github.com/lichess-org/lila-db-seed.git
 git clone https://github.com/lichess-org/fishnet.git --recursive
 git clone https://github.com/lichess-org/lila-fishnet.git
 git clone https://github.com/lichess-org/pgn-viewer.git
+
+ls -al /workspaces
 
 ## Setup initial database and seed test data (users, games, puzzles, etc)
 mkdir -p /workspaces/mongodb-data
